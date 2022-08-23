c2ee33b6749858ea9c336575ddeddfea4e5962e6
Fallet Steve <steve.fallet@divtec.ch>
2022-08-23 16:31:40 +0200

    1st

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Fichier de base pour application web HTML/CSS/JS
+ * @author  Steve Fallet
+ * @version 0.2
+ * @since   2019-08-19
+ */
+
+'use strict'; // Demande une interprétation stricte du code
+
