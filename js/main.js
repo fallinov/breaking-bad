0acbade2c0de876003c28c92ae6f2f0e2286a8e5
Fallet Steve <steve.fallet@divtec.ch>
2022-08-23 16:47:33 +0200

    ajout ESLint

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@
  * @version 0.2
  * @since   2019-08-19
  */
-
 'use strict'; // Demande une interprétation stricte du code
+const axios = require("axios");
 
+
+async function getPersonnages() {
+    try {
+        const response = await axios
+            .get('https://www.breakingbadapi.com/api/characters/');
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+await getPersonnages()
